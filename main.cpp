7ea36c498c5bd6fa53200aa6d5858ddad7ffc8ce
Daniel (dmilith) Dettlaff <dmilith@drakor.eu>
2008-10-07 01:43:18 +0200

    added stubs for debug window. Now it's more clear to know what ekg2-core is doing after init.

--- main.cpp
+++ main.cpp
@@ -46,98 +46,115 @@ extern "C" {
 	}
 
 	static QUERY( qt_ui_is_initialized ) {
+		main_obj->qt_debug_window->append("Ui: Initialized.\n");
 
-		main_obj->qt_main_window->append("dzień dobry\n");
 		return 0;
 	}
 
 	static QUERY( qt_setvar_default ) {
+		main_obj->qt_debug_window->append("Ui: Default variables set.\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_ui_window_switch ) {
+		main_obj->qt_debug_window->append("Ui: Window switched.\n");
 		window_t *w = *(va_arg(ap, window_t **));
 		return 0;
 	}
 
 	static QUERY( qt_ui_window_print ) {
+		main_obj->qt_debug_window->append("Ui: Window print.\n");
 		
 		return 0;
 	}
 
 	static QUERY( qt_ui_window_new ) {
+		main_obj->qt_debug_window->append("Ui: New window.\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_ui_window_kill ) {
+		main_obj->qt_debug_window->append("Ui: Killed window.\n");
 
 		return 0;
 	}
 	
 	static QUERY( qt_ui_window_target_changed ) {
+		main_obj->qt_debug_window->append("Ui: Window target changed.\n");
 
 		return 0;
 	}
 	
 	static QUERY( qt_ui_window_act_changed ) {
+		main_obj->qt_debug_window->append("Ui: Actual window changed.\n");
 		return 0;
 	}
 	
 	static QUERY( qt_ui_window_refresh ) {
+		main_obj->qt_debug_window->append("Ui: Window refresh.\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_ui_window_clear ) {
+		main_obj->qt_debug_window->append("Ui: Window clear.\n");
 
 		return 0;
 	}
 	
 	static QUERY( qt_ui_window_lastlog ) {
+		main_obj->qt_debug_window->append("Ui: Window lastlog.\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_ui_refresh ) {
+		main_obj->qt_debug_window->append("Ui: Refresh.\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_password_input ) {
+		main_obj->qt_debug_window->append("Ui: Password input.\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_statusbar_query ) {
 		window_t *w = *(va_arg(ap, window_t **));
-		main_obj->qt_main_window->append( "Session created" );
+		main_obj->qt_debug_window->append( "Ui: Statusbar query.\n" );
 
 		return 0;
 	}
 
 	static QUERY( qt_binding_set_query ) {
+		main_obj->qt_debug_window->append("Ui: Binding query set (?).\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_binding_adddelete_query ) {
+		main_obj->qt_debug_window->append("Ui: Binding add/delete (?).\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_binding_default ) {
+		main_obj->qt_debug_window->append("Ui: Default binding (?).\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_variable_changed ) {
+		main_obj->qt_debug_window->append("Ui: Variable changed.\n");
 
 		return 0;
 	}
 
 	static QUERY( qt_conference_renamed ) {
+		main_obj->qt_debug_window->append("Ui: Conference renamed.\n");
 
 		return 0;
 	}
@@ -145,6 +162,10 @@ extern "C" {
 	int qt_plugin_init( int prio ) {
 		PLUGIN_CHECK_VER( "qt" );
 		plugin_register( &qt_plugin, prio );
+		lib = new QApplication( argc, argv );
+		main_obj = new Qt4Plugin( "Ekg2" );
+		
+		main_obj->qt_debug_window->append("Ui: Plugin initialized.\n");
 
 		query_connect_id( &qt_plugin, UI_IS_INITIALIZED, qt_ui_is_initialized, NULL );
 		query_connect_id( &qt_plugin, UI_LOOP, qt_plugin_loop, NULL );
@@ -170,8 +191,6 @@ extern "C" {
 		query_connect_id( &qt_plugin, VARIABLE_CHANGED, qt_variable_changed, NULL );
 		query_connect_id( &qt_plugin, CONFERENCE_RENAMED, qt_conference_renamed, NULL );
 		
-		lib = new QApplication( argc, argv );
-		main_obj = new Qt4Plugin( "Ekg2" );
 	
 		#ifdef QT_DEBUG
 			command_exec( NULL, NULL, "/session -a ircnet", 0 );
